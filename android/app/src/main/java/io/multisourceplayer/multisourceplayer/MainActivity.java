09c51f1b9cab9ea2727c7b4d06d9971c77a316c7
crucials <ensueness@gmail.com>
2024-07-25 16:01:46 +0500

    initialize vue capacitor project

---
+++ android/app/src/main/java/io/multisourceplayer/multisourceplayer/MainActivity.java
@@ -0,0 +1,5 @@
+package io.multisourceplayer.multisourceplayer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+    appId: 'io.multisourceplayer.multisourceplayer',
+    appName: 'multi-source-player',
+    webDir: 'dist',
+}
+
+export default config
