ee4a243012f11323a37247bd65ef817f0d2560c4
crucials <ensueness@gmail.com>
2024-08-27 08:51:07 +0500

    reimplement nav bar with vuetify

--- src/widgets/navigation-bar/ui/navigation-bar.vue
+++ src/widgets/navigation-bar/ui/navigation-bar.vue
@@ -4,32 +4,15 @@ import ThemeToggleButton from '@/features/theme-toggle-button/ui/theme-toggle-bu
 </script>
 
 <template>
-    <header class="header">
-        <nav class="navigation">
-            <AccountMenu />
-            <ThemeToggleButton />
-        </nav>
-    </header>
+    <VAppBar class="position-static border-b-thin" elevation="0">
+        <template #append>
+            <nav class="gc-3 d-flex">
+                <AccountMenu />
+                <ThemeToggleButton />
+            </nav>
+        </template>
+    </VAppBar>
 </template>
 
 <style scoped>
-.header {
-    display: flex;
-    align-items: center;
-    gap: 13px;
-    padding: 12px 15px;
-    background-color: rgb(var(--v-theme-surface));
-    border-bottom: 1px solid rgb(var(--v-theme-surface-3));
-}
-
-.navigation {
-    margin-left: auto;
-    display: flex;
-    align-items: center;
-    column-gap: 10px;
-}
-
-.logo {
-    width: 46px;
-}
 </style>
