56be3a6769c9f333f2748c82198ea945f253ee5a
crucials <ensueness@gmail.com>
2024-08-28 11:08:06 +0500

    hide back button if on home page

--- src/widgets/navigation-bar/ui/navigation-bar.vue
+++ src/widgets/navigation-bar/ui/navigation-bar.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import AccountMenu from '@/features/account-menu/ui/account-menu.vue'
 import ThemeToggleButtons from '@/features/theme-toggle-buttons/ui/theme-toggle-buttons.vue'
 
 const router = useRouter()
+const route = useRoute()
 </script>
 
 <template>
     <VAppBar class="position-static border-b-thin" elevation="0">
-        <VBtn icon="mdi-arrow-left" @click="router.back" />
+        <VBtn v-if="route.name !== 'home'" icon="mdi-arrow-left" @click="router.back" />
 
         <template #append>
             <nav class="gc-3 d-flex items-center">
