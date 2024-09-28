7fb9e3f1d4b11874e515b34817cb5cb9b65b54bd
crucials <ensueness@gmail.com>
2024-09-28 20:00:23 +0500

    replace hard-coded auth0 redirect callbacks for web with `window.location.origin`

--- src/shared/config/auth0-redirect-uri.ts
+++ src/shared/config/auth0-redirect-uri.ts
@@ -2,8 +2,8 @@ import { Capacitor } from '@capacitor/core'
 
 const onNativePlatform = Capacitor.isNativePlatform()
 
-export let auth0LogoutRedirectUri = 'http://localhost:5173/home'
-export let auth0AuthCallbackUri = 'http://localhost:5173/auth-callback'
+export let auth0LogoutRedirectUri = `${window.location.origin}/home`
+export let auth0AuthCallbackUri = `${window.location.origin}/auth-callback`
 
 if (onNativePlatform) {
     auth0AuthCallbackUri = 'io.windchimes://auth-callback'
