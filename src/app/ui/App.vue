61ab5aa3339eb8622b898332907fea10e072338b
crucials <ensueness@gmail.com>
2024-09-28 18:14:49 +0500

    add apollo cache clearing after user logging in/out

--- src/app/ui/App.vue
+++ src/app/ui/App.vue
@@ -5,6 +5,7 @@ import NotificationsList from '@/widgets/notifications-list/ui/notifications-lis
 
 import { onErrorCaptured } from 'vue'
 import { storeToRefs } from 'pinia'
+import { useApolloClient } from '@vue/apollo-composable'
 import { App } from '@capacitor/app'
 import { useAuth0 } from '@auth0/auth0-vue'
 import { useFatalErrorStore } from '@/app/model/fatal-error-store'
@@ -14,12 +15,15 @@ const { handleError } = fatalErrorStore
 const { error } = storeToRefs(fatalErrorStore)
 onErrorCaptured(handleError)
 
+const { client: apolloClient } = useApolloClient()
+
 const { handleRedirectCallback, isLoading } = useAuth0()
 
 // listens to redirect to `io.windchimes://auth-callback?state=...&code=....` deep link
 App.addListener('appUrlOpen', async event => {
     if (event.url.includes('state') && event.url.includes('code')) {
         await handleRedirectCallback(event.url)
+        apolloClient.resetStore()
     }
 })
 </script>
