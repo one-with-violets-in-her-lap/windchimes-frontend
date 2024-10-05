9c1ae71f51682caa87a6cb5b7cd4d090e2abf81e
crucials <ensueness@gmail.com>
2024-10-05 14:17:58 +0500

    delete unused personal playlists list component

--- src/widgets/personal-playlists-list/index.ts
+++
@@ -1,4 +0,0 @@
-import { usePlaylistsQuery } from '@/widgets/personal-playlists-list/api/playlists-query'
-import PersonalPlaylistsList from '@/widgets/personal-playlists-list/ui/personal-playlists-list.vue'
-
-export { usePlaylistsQuery, PersonalPlaylistsList }
