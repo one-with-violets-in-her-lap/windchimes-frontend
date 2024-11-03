7199008ec47d64384d6890285a36b0c416e9f193
crucials <ensueness@gmail.com>
2024-11-03 16:45:59 +0500

    swipable elements: fix swipe gesture triggering on vertical scroll

--- src/shared/ui/swipable-element.vue
+++ src/shared/ui/swipable-element.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+
+const POP_UP_WIDTH = 120
+const POP_UP_INITIAL_X_POSITION = -10
+
+/**
+ * maximum of vertical touch offset, the swipe won't trigger when it is reached
+ */
+const MAX_VERTICAL_SWIPE_DISTANCE = 20
+
+/**
+ * minimum of horizontal touch offset after which swipe pop up starts to slide
+ */
+const MIN_HORIZONTAL_SWIPE_DISTANCE = 20
+
+interface Position {
+    x: number
+    y: number
+}
 
 withDefaults(
     defineProps<{
@@ -14,15 +32,53 @@ const emit = defineEmits<{
 
 const swipableElement = ref<HTMLDivElement>()
 
-const touchStartX = ref<number>()
-const touchX = ref<number>()
+const touchStartPosition = ref<Position>()
+const touchPosition = ref<Position>()
 
-function handleTouchMove(eventWrapper: { originalEvent: TouchEvent }) {
-    touchX.value = eventWrapper.originalEvent.touches[0].clientX
+const swipePopUpPosition = computed(() => {
+    if (!touchPosition.value || !touchStartPosition.value) {
+        return POP_UP_INITIAL_X_POSITION
+    }
+
+    const verticalSwipeDistance = Math.abs(
+        touchPosition.value.y - touchStartPosition.value.y,
+    )
+    const horizontalSwipeDistance = Math.abs(
+        touchPosition.value.x - touchStartPosition.value.x,
+    )
+
+    if (
+        verticalSwipeDistance > MAX_VERTICAL_SWIPE_DISTANCE ||
+        horizontalSwipeDistance < MIN_HORIZONTAL_SWIPE_DISTANCE
+    ) {
+        return POP_UP_INITIAL_X_POSITION
+    }
+
+    return Math.min(horizontalSwipeDistance, POP_UP_WIDTH)
+})
+
+function handleTouchMove(event: TouchEvent) {
+    const touch = event.touches[0]
+    touchPosition.value = {
+        x: touch.clientX,
+        y: touch.clientY,
+    }
 }
 
-function handleTouchStart(eventWrapper: { originalEvent: TouchEvent }) {
-    touchStartX.value = eventWrapper.originalEvent.touches[0].clientX
+function handleTouchStart(event: TouchEvent) {
+    const touch = event.touches[0]
+    touchStartPosition.value = {
+        x: touch.clientX,
+        y: touch.clientY,
+    }
+}
+
+function handleTouchEnd() {
+    if (swipePopUpPosition.value >= 80) {
+        emit('swipe')
+    }
+
+    ;[touchPosition.value, touchStartPosition.value] = [undefined, undefined]
 }
 </script>
 
@@ -30,19 +86,16 @@ function handleTouchStart(eventWrapper: { originalEvent: TouchEvent }) {
     <div
         class="position-relative"
         ref="swipableElement"
-        v-touch="{
-            right: () => (swipeEnabled ? emit('swipe') : null),
-            move: handleTouchMove,
-            end: () => ([touchX, touchStartX] = [undefined, undefined]),
-            start: handleTouchStart,
-        }"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+        @touchstart="handleTouchStart"
     >
         <slot></slot>
 
         <div
-            v-show="swipeEnabled && touchStartX"
+            v-show="swipeEnabled && touchStartPosition"
             class="pop-up-on-swipe position-absolute h-100"
-            :style="`left: ${touchX && touchStartX ? Math.min(touchX - touchStartX, 120) + 'px' : '-10px'};`"
+            :style="`left: ${swipePopUpPosition}px;`"
         >
             <slot name="pop-up-on-swipe"></slot>
         </div>
