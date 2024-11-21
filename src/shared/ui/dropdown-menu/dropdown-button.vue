14af325408c895335fa6c4d50dd398ba302e70a3
crucials <ensueness@gmail.com>
2024-11-21 12:54:30 +0500

    add dropdown menu components

---
+++ src/shared/ui/dropdown-menu/dropdown-button.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineOptions({
+    inheritAttrs: false,
+})
+</script>
+
+<template>
+    <VListItem class="p-0">
+        <VBtn variant="text" class="text-capitalize" v-bind="$attrs">
+            <slot></slot>
+        </VBtn>
+    </VListItem>
+</template>
+
+<style scoped></style>
---
+++ src/shared/ui/dropdown-menu/index.ts
@@ -0,0 +1,2 @@
+export { default as DropdownMenu } from '@/shared/ui/dropdown-menu/dropdown-menu.vue'
+export { default as DropdownButton } from '@/shared/ui/dropdown-menu/dropdown-button.vue'
