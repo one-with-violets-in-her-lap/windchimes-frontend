63b975aaac9c2a05c2bc7b41a80319fb1182d743
crucials <ensueness@gmail.com>
2024-11-24 19:15:21 +0500

    complete the signed apk building script

--- build-apk.sh
+++ build-apk.sh
@@ -2,5 +2,7 @@ read -p "keystore path: " keystore_path
 read -p "password: " password
 read -p "alias: " alias
 
+echo ""
+
 npx cap build android --keystorepath $keystore_path --keystorepass \
-    $password --keystorealias $alias --keystorealiaspass $password
+    $password --keystorealias $alias --keystorealiaspass $password --androidreleasetype "APK"
