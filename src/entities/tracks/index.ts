f7149fde12d9a4c4d04631517973f8940665336b
crucials <ensueness@gmail.com>
2024-11-28 08:25:14 +0500

    playlist tracks & queue: move current track thumbnail to `tracks` slice

--- src/entities/tracks/index.ts
+++ src/entities/tracks/index.ts
@@ -1,10 +1,10 @@
 import { queryTrackAudioFile } from '@/entities/tracks/api/audio-file-query'
 import { LOADED_TRACK_FRAGMENT } from '@/entities/tracks/api/loaded-track-fragment'
 import { queryLoadedTrack } from '@/entities/tracks/api/track-query'
-import CurrentTrackThumbnail from '@/entities/tracks/current-track-thumbnail/ui/current-track-thumbnail.vue'
 import type { PlaylistTrack } from '@/entities/tracks/model/track'
-import { TRACKS_PORTION_SIZE } from '@/entities/tracks/utils/tracks-portion-size'
 import TrackItem from '@/entities/tracks/ui/track-item.vue'
+import CurrentTrackThumbnail from '@/entities/tracks/ui/current-track-thumbnail.vue'
+import { TRACKS_PORTION_SIZE } from '@/entities/tracks/utils/tracks-portion-size'
 
 export {
     CurrentTrackThumbnail,
