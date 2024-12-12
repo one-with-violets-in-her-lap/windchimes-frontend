3762a4cf1ff7650d4eb4d1017020b55d5507e6aa
crucials <ensueness@gmail.com>
2024-12-12 15:42:35 +0500

    add jsdoc comments to drag and drop context module

--- src/shared/ui/drag-and-drop/drag-and-drop-context.ts
+++ src/shared/ui/drag-and-drop/drag-and-drop-context.ts
@@ -2,6 +2,11 @@ import { InjectionKey, Ref } from 'vue'
 
 export const ITEM_BASE_ID = 'dragAndDropItem'
 
+/**
+ * state of the drag and drop list
+ * 
+ * shared to child `<DragAndDropItem>` components to handle dragging events
+ */
 export interface DragAndDropContext {
     draggedItemId: Ref<string | null>
 
@@ -10,5 +15,10 @@ export interface DragAndDropContext {
     handleDrag: (itemId: string) => void
 }
 
+/**
+ * the key used for
+ * [vue provide/inject](https://vuejs.org/guide/components/provide-inject.html#provide-inject)
+ * calls to share **{@link DragAndDropContext|drag and drop context}**
+ */
 export const dragAndDropContextProvideKey =
     Symbol() as InjectionKey<DragAndDropContext>
