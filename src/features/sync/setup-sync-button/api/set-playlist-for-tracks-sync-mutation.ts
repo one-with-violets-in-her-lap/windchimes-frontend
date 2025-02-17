563dc0970937998c891173321cfdec6817a924b1
crucials <ensueness@gmail.com>
2025-02-17 18:27:37 +0500

    sync setup: fix invalid fragment usage inh "set playlist for tracks sync" mutation

--- src/features/sync/setup-sync-button/api/set-playlist-for-tracks-sync-mutation.ts
+++ src/features/sync/setup-sync-button/api/set-playlist-for-tracks-sync-mutation.ts
@@ -35,12 +35,16 @@ export function useSetPlaylistForTracksSyncMutation() {
                 }
 
                 ... on ValidationErrorGraphQL {
-                    ...Error
+                    name
+                    explanation
+                    technicalExplanation
                     dotSeparatedFieldLocation
                 }
 
                 ... on ExternalPlaylistNotAvailableErrorGraphQL {
-                    ...Error
+                    name
+                    explanation
+                    technicalExplanation
                 }
             }
         }
