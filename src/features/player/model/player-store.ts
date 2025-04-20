3adff03549f022c5f6eba46ea3b83dad377a47b1
one-with-violets-in-her-lap <walkways@proton.me>
2025-04-20 09:38:25 +0500

    player store: remove useless logging

--- src/features/player/model/player-store.ts
+++ src/features/player/model/player-store.ts
@@ -67,8 +67,6 @@ export const usePlayerStore = defineStore('player', () => {
             return
         }
 
-        console.log(queueItemToPlay.track.platform)
-
         currentQueueItemId.value = queueItemToPlay.id
 
         playAudio(
